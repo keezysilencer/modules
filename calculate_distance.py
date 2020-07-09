67a3ba318538eb29c3fc9713b981fc9137f09e9e
Keezy Silencer <keezyhack@gmail.com>
2020-07-09 16:15:15 +0000

    :nail_care: Add finding nearby location
    Signed-off-by: Keezy Silencer <keezyhack@gmail.com>

--- calculate_distance.py
+++ calculate_distance.py
@@ -15,7 +15,7 @@ class CalculateDistance:
         Calculates the distance between two points on a map using Haversine Formula
         :param location_data: Dict containing the longitude and latitude
         :param destination_data:  Dict containing the longitude and latitude
-        :return: distance between two points in latitude
+        :return: distance between two points in latitude in metres
         """
         # convert decimal degrees to radians
         location_data['longitude'], location_data['latitude'] = map(math.radians, [location_data['longitude'],
@@ -30,3 +30,16 @@ class CalculateDistance:
         c = 2 * math.asin(math.sqrt(a))
 
         return c * self._radius_of_earth
+
+    def nearby_location(self, location_data, destination_data):
+        """
+        Calculates nearby locations within a [radius]
+        :param location_data:     Dict containing the longitude and latitude
+        :param destination_data:  Dict containing the longitude and latitude
+        :return: Bool True when location is within radius and false otherwise
+        """
+        radius = 2000  # 2km
+        if self.distance(location_data, destination_data) <= radius:
+            return True
+        else:
+            return False
