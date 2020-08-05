82f6508fbc63cb1d266041b7edf213e46fe11bc5
Keezy Silencer <keezyhack@gmail.com>
2020-08-05 21:54:21 +0000

    :heavy_division_sign: Refactor distance calculation
    Signed-off-by: Keezy Silencer <keezyhack@gmail.com>

--- calculate_distance.py
+++ calculate_distance.py
@@ -8,7 +8,6 @@ __author__ = "Keezy Silencer"
 
 class CalculateDistance:
     _radius_of_earth = 6371e8  # in metres
-    _distance = {}
 
     def distance(self, location_data, destination_data):
         """
@@ -17,16 +16,17 @@ class CalculateDistance:
         :param destination_data:  Dict containing the longitude and latitude
         :return: distance between two points in latitude in metres
         """
+        distance_data = dict()
         # convert decimal degrees to radians
         location_data['longitude'], location_data['latitude'] = map(math.radians, [location_data['longitude'],
                                                                                    location_data['latitude']])
         destination_data['longitude'], destination_data['latitude'] = map(math.radians, [destination_data['longitude'],
                                                                                          destination_data['latitude']])
         # haversine formula
-        self._distance["longitude"] = destination_data['longitude'] - location_data['longitude']
-        self._distance['latitude'] = destination_data['latitude'] - location_data['latitude']
-        a = math.sin(self._distance['latitude'] / 2) ** 2 + math.cos(location_data['latitude']) * math.cos(
-            destination_data['latitude']) * math.sin(self._distance["longitude"] / 2) ** 2
+        distance_data["longitude"] = destination_data['longitude'] - location_data['longitude']
+        distance_data['latitude'] = destination_data['latitude'] - location_data['latitude']
+        a = math.sin(distance_data['latitude'] / 2) ** 2 + math.cos(location_data['latitude']) * math.cos(
+            destination_data['latitude']) * math.sin(distance_data["longitude"] / 2) ** 2
         c = 2 * math.asin(math.sqrt(a))
 
         return c * self._radius_of_earth
